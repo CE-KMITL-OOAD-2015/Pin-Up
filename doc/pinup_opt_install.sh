63cc13e41c0c7475ba1b55bdb61de61fbbd44796
NatkungZ <natkungzeen@gmail.com>
2015-11-19 23:37:49 +0700

    Rename pinup_opt_start.sh to pinup_opt_install.sh

---
+++ doc/pinup_opt_install.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+#install Tomcat for project Pinup by NKZ@KMITL for Project Pinup Application
+yum install java
+cd /tmp
+wget http://www.eu.apache.org/dist/tomcat/tomcat-7/v7.0.65/bin/apache-tomcat-7.0.65.tar.gz
+tar xzf apache-tomcat-7.0.65.tar.gz
+mv apache-tomcat-7.0.65 /usr/local/tomcat7
+cd /usr/local/tomcat7
+./bin/startup.sh
+firewall-cmd --zone=public --add-port=8080/tcp
+echo "Install Finished"
